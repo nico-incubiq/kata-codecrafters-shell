e02e344cab8a3b1bf78c1ad8f3c56b589c560b7e
Nicolas Séverin <nicolas.severin@outlook.com>
2025-02-04 23:29:17 +0000

    Handle exit 0

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,20 @@ use std::io::{self, Write};
 
 fn main() {
     loop {
-        repl();
+        // Print the prompt.
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+
+        match input.trim() {
+            "exit 0" => break,
+            _ => {
+                eprintln!("{}: command not found", input.trim());
+            }
+        }
     }
-}
-
-fn repl() {
-    // Print the prompt.
-    print!("$ ");
-    io::stdout().flush().unwrap();
-
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
-
-    eprintln!("{}: command not found", input.trim());
 }
