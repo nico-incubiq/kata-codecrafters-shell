1886c3ca348113948ee864f02142d74fd9dd98f1
Nicolas Séverin <nicolas.severin@outlook.com>
2025-02-05 00:24:35 +0000

    Implemented type for files in PATH

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::path::Path;
 
 fn main() {
     loop {
@@ -18,13 +19,29 @@ fn main() {
         match command {
             "exit" if args == "0" => break,
             "echo" => println!("{}", args),
-            "type" => match args {
+            "type" if !args.is_empty() => match args {
                 "exit" | "echo" | "type" => println!("{} is a shell builtin", args),
-                _ => eprintln!("{}: not found", args),
+                _ => type_command(args),
             },
             _ => {
                 eprintln!("{}: command not found", command);
             }
         }
+    }
+}
+
+fn type_command(args: &str) {
+    // Load the PATH env variable.
+    let path = std::env::var("PATH").unwrap();
+    let directories = path.split(':');
+
+    // Check whether the file exists in any of the directories.
+    if let Some(location) = directories
+        .into_iter()
+        .find_map(|dir| Some(Path::new(dir).join(args)).filter(|location| location.exists()))
+    {
+        println!("{} is {}", args, location.display());
+    } else {
+        eprintln!("{}: not found", args);
     }
 }
