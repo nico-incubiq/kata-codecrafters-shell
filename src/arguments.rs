f448ebb52acac14f89576d6fbbab4dddda40e85f
Nicolas Séverin <nicolas.severin@outlook.com>
2025-02-10 00:54:22 +0000

    Implemented escaping outside double-quoted strings

--- src/arguments.rs
+++ src/arguments.rs
@@ -1,30 +1,33 @@
 const ESCAPE_CHARACTER: char = '\\';
-const ESCAPABLE_CHARACTERS: [char; 4] = [DOUBLE_QUOTE, '\\', '$', '\n'];
+const ESCAPABLE_DOUBLE_QUOTED_CHARACTERS: [char; 4] = [DOUBLE_QUOTE, '\\', '$', '\n'];
 const SINGLE_QUOTE: char = '\'';
 const DOUBLE_QUOTE: char = '"';
+const NEWLINE: char = '\n';
 
 pub(crate) fn parse_args(args_string: &str) -> Result<Vec<String>, String> {
     // Split arguments separated by spaces, apart if they are single-quoted.
     let mut split_args = Vec::new();
-    let mut current = String::new();
+    let mut current_arg = String::new();
 
     let mut is_within_quotes = false;
     let mut is_within_double_quotes = false;
     let mut is_escaping = false;
 
     for char in args_string.chars() {
-        if is_arg_boundary(char, &current, is_within_quotes) {
+        if is_arg_boundary(char, &current_arg, is_within_quotes, is_escaping) {
             // Split the argument at this character, skipping the character itself.
-            split_args.push(current);
-            current = String::new();
+            split_args.push(current_arg);
+            current_arg = String::new();
         } else if is_escaping {
-            if !ESCAPABLE_CHARACTERS.contains(&char) {
+            if is_within_double_quotes && !ESCAPABLE_DOUBLE_QUOTED_CHARACTERS.contains(&char) {
                 // Push the escape character.
-                current.push(ESCAPE_CHARACTER);
+                current_arg.push(ESCAPE_CHARACTER);
             }
 
-            // Push the current character.
-            current.push(char);
+            // Push the current character if not a newline.
+            if char != NEWLINE {
+                current_arg.push(char);
+            }
 
             // Disable escape mode.
             is_escaping = false;
@@ -35,17 +38,17 @@ pub(crate) fn parse_args(args_string: &str) -> Result<Vec<String>, String> {
         } else if is_single_quoting_toggle(char, is_within_double_quotes) {
             // Toggle quoted mode.
             is_within_quotes = !is_within_quotes;
-        } else if is_escaping_toggle(char, is_within_double_quotes) {
+        } else if is_escaping_toggle(char) {
             // Enable escape mode.
             is_escaping = true;
         } else if should_capture_char(char, is_within_quotes) {
             // Capture characters.
-            current.push(char);
+            current_arg.push(char);
         }
     }
 
-    if !current.is_empty() {
-        split_args.push(current);
+    if !current_arg.is_empty() {
+        split_args.push(current_arg);
     }
 
     if is_within_quotes {
@@ -60,9 +63,8 @@ fn should_capture_char(current_char: char, is_within_quotes: bool) -> bool {
     is_within_quotes || !current_char.is_whitespace()
 }
 
-fn is_escaping_toggle(current_char: char, is_within_double_quotes: bool) -> bool {
-    // Only interpret backslashes inside a double-quoted string.
-    is_within_double_quotes && current_char == ESCAPE_CHARACTER
+fn is_escaping_toggle(current_char: char) -> bool {
+    current_char == ESCAPE_CHARACTER
 }
 
 fn is_single_quoting_toggle(current_char: char, is_within_double_quotes: bool) -> bool {
@@ -70,14 +72,23 @@ fn is_single_quoting_toggle(current_char: char, is_within_double_quotes: bool) -
     !is_within_double_quotes && current_char == SINGLE_QUOTE
 }
 
-fn is_double_quoting_toggle(current_char: char, is_within_double_quotes: bool, is_within_quotes: bool) -> bool {
+fn is_double_quoting_toggle(
+    current_char: char,
+    is_within_double_quotes: bool,
+    is_within_quotes: bool,
+) -> bool {
     // Only interpret double-quotes if they are not within a single-quoted string.
     (!is_within_quotes || is_within_double_quotes) && current_char == DOUBLE_QUOTE
 }
 
-fn is_arg_boundary(current_char: char, current_arg: &str, is_within_quotes: bool) -> bool {
-    // Break at whitespaces when not within quotes.
-    !is_within_quotes && current_char.is_whitespace() && !current_arg.is_empty()
+fn is_arg_boundary(
+    current_char: char,
+    current_arg: &str,
+    is_within_quotes: bool,
+    is_escaping: bool,
+) -> bool {
+    // Break at whitespaces when not within quotes, and the whitespace is not being escaped.
+    !is_escaping && !is_within_quotes && current_char.is_whitespace() && !current_arg.is_empty()
 }
 
 #[cfg(test)]
@@ -186,11 +197,11 @@ mod tests {
             parse_args(r#"hello "\$HOME""#)
         );
 
-        // Escape newline.
+        // Escape newline, treating it as a continuation.
         assert_eq!(
-            Ok(["hello", "to the\nworld"].map(str::to_owned).to_vec()),
+            Ok(["hello", "to the world"].map(str::to_owned).to_vec()),
             parse_args(
-                r#"hello "to the\
+                r#"hello "to the \
 world""#
             )
         );
@@ -199,6 +210,48 @@ world""#
         assert_eq!(
             Ok(["hello", r#"wor\d"#].map(str::to_owned).to_vec()),
             parse_args(r#"hello "wor\d""#)
+        );
+    }
+
+    #[test]
+    fn it_handles_escaping_outside_double_quotes() {
+        // Escape whitespace.
+        assert_eq!(
+            Ok(["hello   world"].map(str::to_owned).to_vec()),
+            parse_args(r#"hello\ \ \ world"#)
+        );
+
+        // Escape single-quoting.
+        assert_eq!(
+            Ok(["hello", "'world'"].map(str::to_owned).to_vec()),
+            parse_args(r#"hello \'world\'"#)
+        );
+
+        // Escape double-quoting.
+        assert_eq!(
+            Ok(["hello", r#""world""#].map(str::to_owned).to_vec()),
+            parse_args(r#"hello \"world\""#)
+        );
+
+        // Escape newline, treating it as a continuation.
+        assert_eq!(
+            Ok(["hello", "to", "the", "world"].map(str::to_owned).to_vec()),
+            parse_args(
+                r#"hello to \
+the world"#
+            )
+        );
+
+        // Escape backslash.
+        assert_eq!(
+            Ok([r#"he\\o"#, r#"wor\d"#].map(str::to_owned).to_vec()),
+            parse_args(r#"he\\\\o wor\\d"#)
+        );
+
+        // Does NOT print the backslash when not escaping itself.
+        assert_eq!(
+            Ok(["heo", "word"].map(str::to_owned).to_vec()),
+            parse_args(r#"he\o wor\d"#)
         );
     }
 }
